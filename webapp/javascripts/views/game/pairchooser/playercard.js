52a7b8fe4e58f70c781d1e032f094716cc5e35b1
Jacob Burton <jburton@rallydev.com>
2013-03-15 12:00:43 -0600

    adding gravatar to playercard

--- webapp/javascripts/views/game/pairchooser/playercard.js
+++ webapp/javascripts/views/game/pairchooser/playercard.js
@@ -4,8 +4,9 @@ define([
     'underscore',
     'marionette',
     'models/player',
+    'cryptojs',
     'tpl!templates/game/pairchooser/playercard.html'
-], function($, jqueryui, _, Marionette, Player, PlayerCardTpl) {
+], function($, jqueryui, _, Marionette, Player, CryptoJS, PlayerCardTpl) {
     return Marionette.ItemView.extend({
         template: PlayerCardTpl,
         className: 'player-card img-rounded',
@@ -14,6 +15,13 @@ define([
 
         triggers: {
             'drag:dropped': 'drag:dropped'
+        },
+
+        initialize: function(opts) {
+            this.templateHelpers = {
+                imgSize: opts.imgSize || 30,
+                emailHash: CryptoJS.MD5(this.model.get('email').trim().toLowerCase())
+            };
         },
 
         onRender: function() {
